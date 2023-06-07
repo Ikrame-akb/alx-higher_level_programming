aa69591cfb0dddfd5eddd7a52844c3369e594494
Ikrame-akb <ikrame.akabli@gmail.com>
2023-06-07 00:04:07 +0000

    python uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/user/bin/python3
+
+def uppercase(string):
+    for c in string:
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - ord('a') + ord('A'))
+            print("{}".format(c), end="")
+            print()
+                                                
