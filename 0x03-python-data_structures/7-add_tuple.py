20bdde67d24e8bcfb6b3810c90e4d38fc93b153f
Ikrame-akb <ikrame.akabli@gmail.com>
2023-06-13 22:45:31 +0000

    python

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,14 +2,10 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     """Add two tuples."""
-    if len(tuple_a) < 2:
-        if len(tuple_a) == 0:
-            tuple_a = 0, 0
-        else:
-            tuple_a = tuple_a[0], 0
-            if len(tuple_b) < 2:
-                if len(tuple_b) == 0:
-                    tuple_b = 0, 0
-                else:
-                    tuple_b = tuple_b[0], 0
-                    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    # Retrieve the first two elements from each tuple (or use 0 if the tuple is smaller)
+    a = tuple_a[:2] + (0, 0)
+    b = tuple_b[:2] + (0, 0)
+    # Perform element-wise addition and create a new tuple
+    result = (a[0] + b[0], a[1] + b[1])
+    return result
+
