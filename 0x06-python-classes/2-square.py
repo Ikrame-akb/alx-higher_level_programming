6e8d647f9730691342ff4d7ad06ddbd27c1b9987
Ikrame-akb <ikrame.akabli@gmail.com>
2023-06-28 21:20:11 +0000

    python classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,10 +9,11 @@ class Square:
         """Initialize a new Square.
 
     Args:
-    size (int): The size of the new square.
+        size (int): The size of the new square.
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    self.__size = size
+    else:
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,8 +15,9 @@ class Square:
         raise TypeError("size must be an integer")
     elif size < 0:
         raise ValueError("size must be >= 0")
-    self.__size = size
+    else:
+        self.__size = size
 
     def area(self):
         """Return the current area of the square."""
-        return (self.__size * self.__size)
+        return (self.__size ** 2)
