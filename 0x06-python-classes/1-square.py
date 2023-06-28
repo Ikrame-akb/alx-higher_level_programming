ba712cc4d401b6d7d53fded5b24013db23ddfe67
Ikrame-akb <ikrame.akabli@gmail.com>
2023-06-28 21:50:34 +0000

    python classes

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,7 +3,15 @@
 
 
 class Square:
-    """Represent a square."""
+    """Represent a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+
+    Methods:
+        __init__(self, size): Initializes a new instance of the Square class.
+    """
 
     def __init__(self, size):
         """Initialize a new Square.
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,13 +3,24 @@
 
 
 class Square:
-    """Represent a square."""
+    """Represent a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class.
+    """
 
     def __init__(self, size=0):
         """Initialize a new Square.
 
     Args:
         size (int): The size of the new square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
